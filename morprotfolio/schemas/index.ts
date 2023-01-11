0b95140fd7616eb0b902702b78e5515b79c5b539
Mor Amir <imoramira@gmail.com>
2023-01-11 20:38:13 +0200

    sanity schema

--- morprotfolio/schemas/index.ts
+++ morprotfolio/schemas/index.ts
@@ -1,6 +1,9 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+import experience from './experience'
+import pageInfo from './pageInfo'
+import project from './project'
+import skill from './skill'
+import social from './social'
+
+export const schemaTypes = 
+    [pageInfo,skill,experience,project,social]
